47b5ef35685ece2ec9b836df657ddc1b6667a319
Rohit Kumar <kumar25rohit08@gmail.com>
2024-04-06 21:28:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const { checkForAuthenticationCookie } = require("./middleware/authentication");
+const Blog = require("./models/blog");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+mongoose
+      .connect(process.env.MONGO_URL)
+      .then((e) => console.log("MongoDB connected!"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) =>{
+  const allBlogs = await Blog.find({});
+  
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+})
+
+app.use("/user", userRoute);  
+app.use("/blog", blogRoute);  
+
+app.listen(PORT, () => console.log(`Server started At PORT: ${PORT}`));
